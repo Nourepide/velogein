f0c39dc9d54e7f59cc4d4b7718d00b38e9859bcf
Kirill Pixaev <Nourepide@gmail.com>
2022-07-02 18:34:03 +0300

    Init commit

---
+++ build.gradle.kts
@@ -0,0 +1,56 @@
+@file:Suppress("UnstableApiUsage")
+
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("org.springframework.boot") version "2.7.1"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    id("com.vaadin") version "23.1.2"
+    kotlin("jvm") version "1.7.0"
+    kotlin("plugin.spring") version "1.7.0"
+    kotlin("plugin.jpa") version "1.7.0"
+}
+
+group = "net.intervallayers"
+version = "DEV"
+java.sourceCompatibility = JavaVersion.VERSION_18
+
+repositories {
+    mavenCentral()
+}
+
+extra["vaadinVersion"] = "23.1.2"
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.vaadin:vaadin-spring-boot-starter")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+    runtimeOnly("org.postgresql:postgresql")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("com.vaadin:vaadin-bom:${property("vaadinVersion")}")
+    }
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "18"
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ src/main/kotlin/net/intervallayers/velogein/VelogeinApplication.kt
@@ -0,0 +1,11 @@
+package net.intervallayers.velogein
+
+import org.springframework.boot.*
+import org.springframework.boot.autoconfigure.*
+
+@SpringBootApplication
+class VelogeinApplication
+
+fun main(args: Array<String>) {
+    runApplication<VelogeinApplication>(*args)
+}
